64708e510ec4fd096818a70b6be1af79ba0e9c07
syslink <7135202@qq.com>
2022-10-14 20:26:23 +0800

    update address

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
-const xenAddr: Record<number, string> = {1: '0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8', 56: 'BNB', 97: '0x64AA3cc1348329F175ABE7f4b6F779Df4605C266', 137: 'Polygon'}
-const xNFTAddr: Record<number, string> = {1: '', 56: 'BNB', 97: '0x650024868692eD29c2b6dFC87ffEaE12eD3a91F9', 137: 'Polygon'}
-const rewardCalculatorAddr: Record<number, string> = {1: '', 56: 'BNB', 97: '0x5c73A8996F2B4390FEe9D1912e8f0070b0665D05', 513100: '', 137: 'Polygon'}
+const xenAddr: Record<number, string> = {1: '0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8', 56: 'BNB', 97: '0xa525366969F79b223a5d16900e8F0754c64C3EDa', 137: 'Polygon'}
+const xNFTAddr: Record<number, string> = {1: '', 56: '', 97: '0x77dfB189A25514b4087D499fe9BCaaDABD87b5D4', 137: ''}
+const rewardCalculatorAddr: Record<number, string> = {1: '', 56: '', 97: '0x650024868692eD29c2b6dFC87ffEaE12eD3a91F9', 513100: '', 137: ''}
 const chainId2NetworkName: Record<number, string> = {1: 'Ethereum', 56: 'BSC', 97: 'BSC-Testnet', 137: 'Polygon', 80001: 'Mumbai'}
 const unit: Record<number, string> = {1: 'ETH', 56: 'BNB', 97: 'BNB', 137: 'MATIC', 80001: 'MATIC', 10001: 'ETHW', 513100: 'ETF'}
 
